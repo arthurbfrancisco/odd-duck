70a48cdb7b9d71806701f40ee4ec8f20be86d881
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-19 04:21:45 +0700

    update

--- README.md
+++ README.md
@@ -4,17 +4,14 @@
 I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
 
 ### Author: Arthur F
-credit: Dan, Anthony, Jared
+Lab11-credit: Dan, Anthony, Jared
+Lab12-credit:
 
 ### Links and Resources
-
-
 * Any Links you used as reference
 
-### Lighthouse Accessibility Report Score
-* [Lighthouse score]()
-* Provide a screenshot of your score after running a Lighthouse Accessibility report.
-
+### Lighthouse Accessibility Report Score:
+* [Lighthouse score_Lab-11](image/lighthouse_lab11.png)
 ### Reflections and Comments
 
 * JS DOM
--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 'use strict';
-
+// Declare an array to store duck objects
 let duckArray = [];
-
+// Get a reference to the section element in the DOM
 let myContainer = document.querySelector('section');
-
+// Get references to the first, second and third image elements inside the section
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
-
+// Get a reference to the view results button
 let viewResultsBtn = document.querySelector('section ~ div')
-
+// Initialize a counter for the number of clicks and the 
 let counter = 0;
 let maxCounter = 25; //25
-
+// Define the Duck constructor , declare the default value
 function Duck(name, fileExtension = 'jpg') {
     this.name = name;
     this.src = `image/${name}.${fileExtension}`;
     this.views = 0;
     this.votes = 0;
 }
-
+// Create new Duck objects
 let bags = new Duck('bag');
 let bananacutter = new Duck('banana');
 let bathroomholder = new Duck('bathroom');
@@ -38,28 +38,30 @@ let tauntaunblanket = new Duck('tauntaun');
 let cannedunicorn = new Duck('unicorn');
 let pitchwater = new Duck('watercan');
 let glasswine = new Duck('wineglass');
-
-duckArray.push(bags, bananacutter, bathroomholder, bootsyellow, breakfastoven, meatballbubblegum, redhair, monstercthulhu, dogmouth, meatdragon, pens, sweeper, scissorcutter,  sharkpillow, sharkpillow, tauntaunblanket, cannedunicorn, pitchwater,glasswine,)    ;
-
+// Add the created Duck objects to the duckArray, instances push to the array
+duckArray.push(bags, bananacutter, bathroomholder, bootsyellow, breakfastoven, meatballbubblegum, redhair, monstercthulhu, dogmouth, meatdragon, pens, sweeper, scissorcutter, sharkpillow, sharkpillow, tauntaunblanket, cannedunicorn, pitchwater, glasswine,);
 // console.log(duckArray);
-
+// Function to render ducks on the page
 // - get a random number to use with duck array to get a random duck
-function renderDucks(){
+
+function renderDucks() {
     // let duck1 = selectRandomDuckNumber();
     // let duck2 = selectRandomDuckNumber();
-    // let duck3 = selectRandomDuckNumber();
+    // let duck3 = selectRandomDuckNumber();  
     // console.log(duck1, duck2, duck3);
     let selectedImages = [];
-    while(selectedImages.length < 3){
-     let randomIndex = Math.floor(Math.random() * duckArray.length);
-     if (!selectedImages.includes(randomIndex)) {
-        selectedImages.push(randomIndex);
-      }
-    //   console.log(randomIndex);
+    while (selectedImages.length < 3) {
+        let randomIndex = Math.floor(Math.random() * duckArray.length);
+        //haystack.push(randomIndex, duckArray[randomIndex]);
+        if (!selectedImages.includes(randomIndex)) {
+            selectedImages.push(randomIndex);
+        }
     }
-let imageOneIndex = selectedImages.shift();
-let imageTwoIndex = selectedImages.shift();
-let imageThreeIndex = selectedImages.shift();
+    //   console.log(randomIndex);
+
+    let imageOneIndex = selectedImages.shift(); //25
+    let imageTwoIndex = selectedImages.pop(); //25
+    let imageThreeIndex = selectedImages.shift(); //25
 
     image1.src = duckArray[imageOneIndex].src;
     image1.alt = duckArray[imageOneIndex].name;
@@ -70,17 +72,17 @@ let imageThreeIndex = selectedImages.shift();
     image3.src = duckArray[imageThreeIndex].src;
     image3.alt = duckArray[imageThreeIndex].name;
     duckArray[imageThreeIndex].views++;
-    }
-
+}
+// Function to handle clicks on duck images, event listener.
 function handleDuckClick(event) {
     counter++;
     console.log(event.target.alt);
     let clickedDuck = event.target.alt;
     // find a the goat instance in the goat array whose name property equals the clickedGoat value.
     for (let i = 0; i < duckArray.length; i++) {
-        if (clickedDuck === duckArray[i].name) {
-            duckArray[i].votes++;
-            console.log(duckArray);
+        if (clickedDuck === duckArray[i].name) { // if the duck is already  clicked then just return.
+            duckArray[i].votes++;   // increment
+            console.log(duckArray); // increment  the number of          
         }
     }
     // check to see if the round has ended
@@ -91,11 +93,13 @@ function handleDuckClick(event) {
         // After voting rounds have been completed, remove the event listeners on the product.
         myContainer.removeEventListener('click', handleDuckClick);
         // make the button clickable
-        viewResultsBtn.addEventListener('click', viewResults);
+        viewResultsBtn.addEventListener('click', viewResults); //invoke the view result
         // stop the game and render the results
+
+        renderCharts();
     }
 }
-
+// Function to display the results after voting rounds are completed
 function viewResults() {
     let ul = document.querySelector('ul');
     for (let i = 0; i < duckArray.length; i++) {
@@ -104,11 +108,91 @@ function viewResults() {
         ul.appendChild(li);
     }
 
+
 }
+// Call renderDucks function to display initial ducks
 
-renderDucks();
 
-// event listener on myContainer, for clicks,
-// my event handler is handleGoatClick
 
-myContainer.addEventListener('click', handleDuckClick);
+// Add an event listener to handle clicks on the duck images
+//  function renderCharts
+function renderCharts() {
+    // console.log(duckArray);
+    // Charts
+    const ctx = document.getElementById('myChart'); //window to the DOM element
+
+
+    let duckNames = [];
+    let duckVotes = [];
+    let duckViews = [];
+
+    for (let i = 0; i < duckArray.length; i++) {
+        console.log(duckArray[i]);
+
+        let name = duckArray[i].name;
+        duckNames.push(name);
+
+        duckVotes.push(duckArray[i].vote);
+        duckViews.push(duckArray[i].view);
+        console.log(duckNames);
+        console.log(duckVotes);
+        console.log(duckViews); 
+
+        }
+
+
+        let config = {
+            type: 'bar',
+            data: {
+                labels: duckNames,
+
+                datasets: [
+
+                    {
+                        label: '# of Votes',
+                        data: duckVotes,
+                        borderWidth: 1,
+                    },
+                    {
+                        label: '# of Views',
+                        data: duckViews,
+                        borderWidth: 1,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(201, 203, 207, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)',
+                            'rgb(153, 102, 255)',
+                            'rgb(201, 203, 207)'
+                        ],
+                    }
+
+                ],
+
+            },
+
+
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+        new Chart(ctx, config);
+    };
+
+
+    renderDucks();
+    myContainer.addEventListener('click', handleDuckClick);
